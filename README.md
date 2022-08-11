7a580b8bdf88625201c9ce904011e151c8303821
Francisco Castanheira <franciscocastanheira1998@gmail.com>
2022-08-11 15:41:00 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Asynchronous Server
+
+Ideas:
+- `top`-like command, that says how many clients are using the server.
+- Supporting tcp and udp clients.
+- Voice udp broadcast or multicast.
---
+++ build.sh
@@ -0,0 +1,14 @@
+rm -r build
+
+mkdir build
+
+cd build || {
+  echo 'Entry into build folder failed.'
+  exit 1
+}
+
+cmake -DCMAKE_PREFIX_PATH="${CONDA_PREFIX}" \
+      ..
+
+cmake --build . -- -j 4
+
